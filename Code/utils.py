6e35566f0e68ad7d40b9727f003dcb3083a34002
unknown <aguedi17@gmail.com>
2023-01-30 20:59:28 +0100

    Create sum_mines function

--- Code/utils.py
+++ Code/utils.py
@@ -19,4 +19,32 @@ def print_pboard(pboard):
             s+=" "
         print(s)
 
-print_pboard(test_board)
+# print_pboard(test_board)
+
+def sum_mines(g_board):
+    x_len = len(g_board)
+    y_len = len(g_board[0])
+    s_board = [[0  for j in range(y_len)] for i in range(x_len)]
+    for i in range(x_len):
+        for j in range(y_len):
+            if g_board[i][j]==1:
+                s_board[i][j]=-1
+                continue
+            counter=0
+            for i_add in [-1, 0, 1]:
+                for j_add in [-1, 0, 1]:
+                    if i+i_add>=0 and i+i_add<x_len and j+j_add>=0 and j+j_add<y_len:
+                        counter=counter+g_board[i+i_add][j+j_add]
+                    if i_add==0 and j_add==0:
+                        counter=counter-g_board[i+i_add][j+j_add]
+                        continue
+
+            s_board[i][j]=counter
+    return(s_board)
+
+# test_board_2 = [[1,1,0,0], [0,1,0,0], [0,0,1,0],[0,0,0,1]]
+# for i in range(len(test_board_2)):
+#     print(test_board_2[i])
+# summed_board=sum_mines(test_board_2)
+# for i in range(len(test_board_2)):
+#     print(summed_board[i])
