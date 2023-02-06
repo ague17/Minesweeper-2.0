e5cf90040fc955dc92c2a5ad277c712f069f36ab
unknown <aguedi17@gmail.com>
2023-02-06 19:12:17 +0100

    polish game loop and fix bugs

--- Code/main.py
+++ Code/main.py
@@ -4,12 +4,6 @@ import numpy as np
 from utils import *
 import tkinter as tk
 
-tablero = []
-def which_button(button_press):
-    x, y = button_press
-    explore(x, y, s_board, p_board, tablero)
-
-mode=input("Choose mode: (e=easy, m=medium, h=hard): ")
 E_SIZE=10
 M_SIZE=20
 H_SIZE=50
@@ -17,24 +11,85 @@ C_SIZE=0
 M_DENS=0.08
 M_DENS_TOL=0.02
 
-#set C_SIZE (chosen size)
-if mode=="e": C_SIZE=E_SIZE
-elif mode=="m": C_SIZE=M_SIZE
-elif mode=="h": C_SIZE=H_SIZE
-else:
-    print("Invalid mode chosen. Quitting")
-    quit()
+
+tablero = []
+exploradas = 0
+num_mines = 0
+s_board = []
+p_board = []
+def which_button(button_press):
+    global exploradas
+    global root
+    global s_board
+    global p_board
+    global tablero
+    x, y = button_press
+    exploradas+=explore(x, y, s_board, p_board, tablero)
+    if exploradas+num_mines>=C_SIZE*C_SIZE:
+        print("Ganado supuestamente")
+        root.destroy()
+    if s_board[x][y]=="X":
+        print("Perdido supuestamente")
+        root.destroy()
+
+def choose_dif(mode, frame):
+    #set C_SIZE (chosen size)
+    global C_SIZE
+    global C_SIZE
+    global C_SIZE
+    global C_SIZE
+    if mode=="e": C_SIZE=E_SIZE
+    elif mode=="m": C_SIZE=M_SIZE
+    elif mode=="h": C_SIZE=H_SIZE
+    else:
+        print("Invalid mode chosen. Quitting")
+        quit()
+    frame.destroy()
+    game_start()
+
+
+def game_start():
+
+    global C_SIZE
+    global num_mines
+    global p_board
+    global s_board
+
+    while num_mines>(M_DENS+M_DENS_TOL)*C_SIZE*C_SIZE or num_mines<(M_DENS-M_DENS_TOL)*C_SIZE*C_SIZE:
+        g_board = [[(1 if random()<M_DENS else 0) for j in range(C_SIZE)] for i in range(C_SIZE)]
+        #count mines
+        num_mines=np.sum(g_board)
+        print("Number of mines: ", num_mines)
+
+    p_board = [["■" for j in range(C_SIZE)] for i in range(C_SIZE)]
+    s_board = sum_mines(g_board)
+
+    #frames (like areas to place stuff)
+    mainFrame = tk.Frame()
+    mainFrame.pack(expand=1) #Configurar el metodo pack()
+    # mainFrame.config(bg="blue")
+    mainFrame.config(width="150", height="150")
+
+    #place thing in frame:
+    for i in range(C_SIZE):
+        fila_tablero = []
+        for j in range(C_SIZE):
+            button = tk.Button(mainFrame, text="  ", font=("Arial",10), \
+            command=lambda m=[i,j]: which_button(m))
+            fila_tablero.append(button)
+            button.grid(row=j, column=i)
+        tablero.append(fila_tablero)
+    #run root
+
+    print("Después de main loop")
+    # for i in range(C_SIZE):
+    #     print(board[i])
+
+    #Timer
+    iTime=time()
 
 #Initialize boards, "game" and "player" boards
-num_mines=0
-while num_mines>(M_DENS+M_DENS_TOL)*C_SIZE*C_SIZE or num_mines<(M_DENS-M_DENS_TOL)*C_SIZE*C_SIZE:
-    g_board = [[(1 if random()<M_DENS else 0) for j in range(C_SIZE)] for i in range(C_SIZE)]
-    #count mines
-    num_mines=np.sum(g_board)
-    print("Number of mines: ", num_mines)
-
-p_board = [["■" for j in range(C_SIZE)] for i in range(C_SIZE)]
-s_board = sum_mines(g_board)
+print("csize is", str(C_SIZE))
 
 #init app after setting difficulty (TB changed)
 #conventional window name
@@ -50,31 +105,25 @@ label.pack(padx=0,pady=10)
 label = tk.Label(root, text="Developed by\nague17 & Nerocraft4 ",\
                  font=('Arial',8))
 label.pack()
+dif_frame = tk.Frame()
+dif_frame.pack(expand=1)
+dif_frame.config(width="100", height="20")
 
-#frames (like areas to place stuff)
-mainFrame = tk.Frame()
-mainFrame.pack(expand=1) #Configurar el metodo pack()
-# mainFrame.config(bg="blue")
-mainFrame.config(width="150", height="150")
+#easy button
+button_e = tk.Button(dif_frame, text="EASY", font=("Arial",10), \
+command=lambda m="e": choose_dif(m, dif_frame))
+button_e.grid(column=0, row=0, padx=(10,10))
+#medium button
+button_m = tk.Button(dif_frame, text="MEDIUM", font=("Arial",10), \
+command=lambda m="m": choose_dif(m, dif_frame))
+button_m.grid(column=1, row=0, padx=(10,10))
+#hard button
+button_h = tk.Button(dif_frame, text="HARD", font=("Arial",10), \
+command=lambda m="h": choose_dif(m, dif_frame))
+button_h.grid(column=2, row=0, padx=(10,10))
 
-#place thing in frame:
-C_SIZE = 10
-for i in range(C_SIZE):
-    fila_tablero = []
-    for j in range(C_SIZE):
-        button = tk.Button(mainFrame, text="  ", font=("Arial",10), \
-        command=lambda m=[i,j]: which_button(m))
-        fila_tablero.append(button)
-        button.grid(row=j, column=i)
-    tablero.append(fila_tablero)
-#run root
 root.mainloop()
 
-# for i in range(C_SIZE):
-#     print(board[i])
-
-#Timer
-iTime=time()
 '''
 #states
 discovered_cells=0
